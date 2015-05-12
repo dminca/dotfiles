bc5a9eb9c84cd6d6f559675862e555fee7af78dd
Minca Daniel Andrei <mandrei17@gmail.com>
2015-05-12 14:55:18 +0000

    environment.sh created online with Bitbucket

---
+++ environment.sh
@@ -0,0 +1,17 @@
+####################################################
+##### SETUP MY DEVELOPMENT ENVIRONMENT #############
+######### By Daniel aka Daniel Bourne ##############
+####################################################
+
+##### DOWNLOAD LINUX
+
+# Linux versions
+# http://old-releases.ubuntu.com/releases/10.04.3/
+
+# Download the ISO
+# http://old-releases.ubuntu.com/releases/10.04.3/ubuntu-10.04.4-desktop-amd64.iso
+
+# Download via Torrent
+# http://old-releases.ubuntu.com/releases/10.04.3/ubuntu-10.04.4-desktop-amd64.iso.torrent
+
+# --------------------------------------------------------------------------------
