f903e92046d95285dea1e05ec37957700ba24bc0
Daniel Minca <dminca@marketstheworld.com>
2015-05-19 11:41:41 +0300

    Launch MarketsWorld engines in tmux

---
+++ sandbox/ngin_launch.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Launch MarketsWorld engines
+
+# Create window 1
+	# Run server thin
+	# Add horizontal pane
+	#	Run console on sandbox mode
+	# Switch to right pane
+	# Run DB console
+
+# Create window 2
+	# cd in stream-dispatcher
+	# Run node server.js
+
+	# Add vertical pane
+	# cd in mwd-comm
+	# run node app.js
+
+	# Add horizontal pane
+	# cd in market data
+	# Run bundle exec bin/market-data
+
+	# Add vertical pane
+	# cd in data-retention
+	# Run bundle exec bin/...
+
+	# Add horizontal pane
+	# cd game
+	# Run bundle exec bin/..
+	
+	# Add vertical pane
+	# cd resolve
+	# Run bundle exec bin/...
+
+	# Add horizontal pane
+	# cd expiry
+	# Run bundle exec bin/..
+
+	# Add vertical pane
+	# cd watchdog
+	# Run bundle exec bin/...
