97d2cab9ef285482737fb4c8ee70270a787240f1
Minca Daniel <andrei_17_09@hotmail.com>
2015-07-26 12:21:53 +0300

    Init drush project
    Day 0 of 'drush' zsh plugin.
    This plugin is ment to simplify Drupal developers and themers lives.
    Anyone is welcome to join the project as long as they follow this
    principle:
    - KEEP IT SIMPLE STUPID

---
+++ custom/plugins/drush/drush.plugin.zsh
@@ -0,0 +1,30 @@
+#  _____                 _     
+# |  __ \               | |    
+# | |  | |_ __ _   _ ___| |__  
+# | |  | | '__| | | / __| '_ \ 
+# | |__| | |  | |_| \__ \ | | |
+# |_____/|_|   \__,_|___/_| |_|
+#                              
+
+# Aliases
+alias dr='drush'
+
+alias dh='drush help'
+
+alias dard='drush ard'
+alias darb='drush arb'
+alias darr='drush arr'
+alias dbro='drush browse'
+
+alias ducrt='drush ucrt'
+
+alias dphp='drush php'
+alias dconf='drush conf'
+alias dcron='drush cron'
+
+alias dcca='drush cc all'
+
+alias dsqlc='drush sqlc'
+
+alias dwdl='drush wd-list'
+alias dwds='drush wd-show'
