bad1f4b2fa306da087b02150337d4dcf9f2b0489
Minca Daniel Andrei <mandrei17@gmail.com>
2016-03-21 09:56:35 +0200

    Modify informational text
    Signed-off-by: Minca Daniel Andrei <mandrei17@gmail.com>

--- netconn.sh
+++ netconn.sh
@@ -9,7 +9,7 @@ light_red='\e[1;91m%s\e[0m\n'
 light_green='\e[1;92m%s\e[0m\n'
 ping -c 4 -q google.com
 if [ "$?" -eq 0 ]; then
-  printf "$light_green" "[ CONNECTION AVAILABLE ]"
+  printf "$light_green" "[ ONLINE ]"
 else
-  printf "$light_red" "[ HOST DISCONNECTED ]"
+  printf "$light_red" "[ OFFLINE ]"
 fi
